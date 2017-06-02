1eb4b556112138dec77298ef725ecf59b24c01ea
Kris Sankaran <sankaran.kris@gmail.com>
2017-06-02 11:51:07 -0700

    write all figures to file

--- src/antibiotic.R
+++ src/antibiotic.R
@@ -17,6 +17,8 @@ theme_set(ggscaffold::min_theme(list(
                         "border_size" = 0.8
                       ))
           )
+figure_dir <- file.path("..", "doc", "figure")
+dir.create(figure_dir)
 
 ## --- utils ----
 melted_counts <- function(x) {
@@ -62,10 +64,9 @@ join_sources <- function(x, taxa, samples, dendro, h = 0.5) {
 
   mx %>%
     group_by(sample, cluster) %>%
-    mutate(
-      centroid = median(scaled),
-      centroid_prob = mean(value)
-    )
+    mutate(centroid_prob = mean(present)) %>%
+    group_by(sample, cluster, present) %>%
+    mutate(centroid = mean(scaled))
 }
 
 combined_heatmap <- function(mx, fill_type = "bw") {
@@ -115,18 +116,46 @@ combined_heatmap <- function(mx, fill_type = "bw") {
 }
 
 centroid_plot <- function(mx) {
-  ggplot(mx) +
+  centroid_data <- mx %>%
+    group_by(sample, cluster) %>%
+    summarise(time = time[1], ind = ind[1], centroid = centroid[1], centroid_prob = centroid_prob[1])
+
+  p1 <- ggplot(mx) +
     geom_point(
       aes(x = time, y = scaled, col = ind), size = 0.2, alpha = 0.2
     ) +
     geom_point(
+      data = centroid_data,
       aes(x = time, y = centroid), size = 0.7
     ) +
     geom_point(
-      aes(x = time, y = centroid, group = rsv, col = ind), size = 0.4
+      data = centroid_data,
+      aes(x = time, y = centroid, col = ind), size = 0.4
     ) +
     scale_color_brewer(palette = "Set1") +
     facet_wrap(~cluster)
+
+  p2 <- ggplot(mx) +
+    geom_point(
+      aes(x = time, y = present, col = ind),
+      size = 0.2, alpha = 0.2,
+      position = position_jitter(height = 0.2)
+    ) +
+    geom_point(
+      data = centroid_data,
+      aes(x = time, y = centroid_prob), size = 0.7
+    ) +
+    geom_point(
+      data = centroid_data,
+      aes(x = time, y = centroid_prob, col = ind), size = 0.4
+    ) +
+    scale_color_brewer(palette = "Set1") +
+    facet_wrap(~cluster)
+
+  list(
+    "conditional" = p1,
+    "presence" = p2
+  )
 }
 
 ## ---- data ----
@@ -171,8 +200,14 @@ taxa <- abt %>%
 mix_dendro <- reorder(as.dendrogram(mix_tree), -colMeans(x))
 mx <- join_sources(x, taxa, samples, mix_dendro, h = 0.5)
 sort(table(mx$cluster), decreasing = TRUE) / nrow(mx)
-combined_heatmap(mx)
-centroid_plot(mx)
+
+
+## 4.5, 6.5 and 4, 6.5
+all_plots <- list()
+all_plots[["heatmap-mix"]] <- combined_heatmap(mx)
+p <- centroid_plot(mx)
+all_plots["centroid-mix-conditional"] <- p$conditional
+all_plots["centroid-mix-presence"] <- p$presence
 
 ## ---- heatmap-extremes ----
 alpha <- 0
@@ -181,17 +216,21 @@ tree <- hclust(D_mix, method = "complete")
 dendro <- reorder(as.dendrogram(tree), -colMeans(x))
 mx <- join_sources(x, taxa, samples, dendro, h = 0.2)
 sort(table(mx$cluster), decreasing = TRUE) / nrow(mx)
-combined_heatmap(mx)
-centroid_plot(mx)
+all_plots[["heatmap-euclidean"]] <- combined_heatmap(mx)
+p <- centroid_plot(mx)
+all_plots["centroid-euclidean-conditional"] <- p$conditional
+all_plots["centroid-euclidean-presence"] <- p$presence
 
 alpha <- 1
 D_mix <- alpha * D_jaccard + (1 - alpha) * D_euclidean
 tree <- hclust(D_mix)
 dendro <- reorder(as.dendrogram(tree), -colMeans(x))
-mx <- join_sources(x, taxa, samples, dendro)
+mx <- join_sources(x, taxa, samples, dendro, h = 0.9)
 sort(table(mx$cluster), decreasing = TRUE) / nrow(mx)
-combined_heatmap(mx)
-centroid_plot(mx)
+all_plots[["heatmap-jaccard"]] <- combined_heatmap(mx)
+p <- centroid_plot(mx)
+all_plots["centroid-jaccard-conditional"] <- p$conditional
+all_plots["centroid-jaccard-presence"] <- p$presence
 
 ## ---- innovations ----
 diff_x <- apply(x_scaled, 2, diff)
@@ -199,7 +238,10 @@ tree <- hclust(dist(t(diff_x)))
 dendro <- reorder(as.dendrogram(tree), -var(diff_x))
 mx <- join_sources(diff_x, taxa, samples, dendro, h = 0.15)
 sort(table(mx$cluster), decreasing = TRUE) / nrow(mx)
-combined_heatmap(mx, "gradient2")
+all_plots[["heatmap-innovations"]] <- combined_heatmap(mx, "gradient2")
+p <- centroid_plot(mx)
+all_plots["centroid-innovations-conditional"] <- p$conditional
+all_plots["centroid-innovations-presence"] <- p$presence
 
 ## ---- innovations-bin ----
 diff_x <- apply(x_bin, 2, diff)
@@ -207,7 +249,10 @@ tree <- hclust(dist(t(diff_x), method = "Jaccard"))
 dendro <- reorder(as.dendrogram(tree), -var(diff_x))
 mx <- join_sources(diff_x, taxa, samples, dendro, h = 0.985)
 sort(table(mx$cluster), decreasing = TRUE) / nrow(mx)
-combined_heatmap(mx, "gradient2")
+all_plots[["heatmap-innovations-bin"]] <- combined_heatmap(mx, "gradient2")
+p <- centroid_plot(mx)
+all_plots["centroid-innovations-bin-conditional"] <- p$conditional
+all_plots["centroid-innovations-bin-presence"] <- p$presence
 
 ## ---- pacf ----
 pacfs <- t(apply(x_scaled, 2, function(x) { pacf(x, plot = FALSE)$acf[, 1, 1] }))
@@ -219,7 +264,7 @@ pacfs <- pacfs %>%
   mutate(lag = as.integer(gsub("X", "", lag))) %>%
   left_join(taxa)
 
-ggplot(pacfs) +
+all_plots[["pacf"]] <- ggplot(pacfs) +
   geom_line(
     aes(x = lag, y = value, group = rsv, col = label),
     alpha = 0.2, size = 0.4
@@ -244,7 +289,7 @@ x_pairs <- cbind(x_scaled, x_offset) %>%
   left_join(samples) %>%
   left_join(taxa)
 
-ggplot(x_pairs %>% filter(time >= 10, time <= 20)) +
+all_plots[["pacf_pairs"]] <- ggplot(x_pairs %>% filter(time >= 10, time <= 20)) +
   geom_abline(slope = 1, size = 1, alpha = 0.3) +
   geom_point(
     aes(x = pre, y = post, col = label),
@@ -265,7 +310,7 @@ plot_hmm <- cbind(df, fm@posterior) %>%
   left_join(samples) %>%
   gather(state_prob, prob, S1, S2, S3, S4)
 
-ggplot(plot_hmm) +
+all_plots[["hmm-example"]] <- ggplot(plot_hmm) +
   geom_line(aes(x = time, y = y, col = ind), size = 0.4, alpha = 0.3) +
   geom_point(aes(x = time, y = y, col = ind, size = prob)) +
   facet_grid(state_prob ~ .) +
@@ -273,51 +318,60 @@ ggplot(plot_hmm) +
   scale_color_brewer(palette = "Set1")
 
 ## ---- parallel-hmm ----
-plot_hmm <- list()
-K <- 4
+## plot_hmm <- list()
+## K <- 4
 
-## Loop over every RSV and fit an HMM
-for (i in seq_len(ncol(x_scaled))) {
-  if (i %% 10 == 0) {
-    cat(sprintf("HMM on rsv %s\n", i))
-  }
+## ## Loop over every RSV and fit an HMM
+## for (i in seq_len(ncol(x_scaled))) {
+##   if (i %% 10 == 0) {
+##     cat(sprintf("HMM on rsv %s\n", i))
+##   }
 
-  df <- data_frame(
-    "sample" = rownames(x_scaled),
-    "y" = x_scaled[, i] + runif(nrow(x_scaled), 0, 0.005),
-    "rsv" = colnames(x_scaled)[i]
+##   df <- data_frame(
+##     "sample" = rownames(x_scaled),
+##     "y" = x_scaled[, i] + runif(nrow(x_scaled), 0, 0.005),
+##     "rsv" = colnames(x_scaled)[i]
+##   )
+
+##   msp <- depmix(
+##     y ~ 1,
+##     nstates = K,
+##     data = df,
+##     emcontrol = em.control(classification = "soft")
+##   )
+##   fm <- try(fit(msp, verbose = FALSE))
+
+##   if (class(fm) != "try-error") {
+##     ## try to align states across rsvs, naive approach just sorts by emission mean
+##     state_order <- c(1, 1 + order(summary(fm)[, 1], decreasing = TRUE))
+##     ordered_posterior <- fm@posterior[, state_order]
+##     colnames(ordered_posterior) <- c("state", paste0("S", seq_len(K)))
+
+##     plot_hmm[[i]] <- cbind(df, ordered_posterior) %>%
+##       gather(state_prob, prob, starts_with("S", ignore.case = FALSE)) %>%
+##       as_data_frame
+##   }
+## }
+
+## plot_hmm <- do.call(rbind, plot_hmm) %>%
+##   left_join(samples)
+## plot_hmm$rsv <- factor(plot_hmm$rsv, levels = mix_tree$label)
+
+## for (state in paste0("S", 1:K)) {
+##   all_plots[[sprintf("hmm-%s", state)]] <- ggplot(plot_hmm %>% filter_(sprintf("state_prob == '%s'", state))) +
+##     geom_tile(aes(x = rsv, y = sample, fill = prob)) +
+##     scale_fill_gradient(low = "white", high = "black") +
+##     facet_grid(ind ~ ., scale = "free_y") +
+##     theme(axis.text = element_blank()) +
+##     ggtitle(sprintf("State %s", state))
+## }
+
+## write all the plots to file
+for (i in seq_along(all_plots)) {
+  ggsave(
+    file.path(figure_dir, paste0(names(all_plots)[i], ".png")),
+    all_plots[[i]],
+    dpi = 450,
+    height = 3, width = 6.5
   )
-
-  msp <- depmix(
-    y ~ 1,
-    nstates = K,
-    data = df,
-    emcontrol = em.control(classification = "soft")
-  )
-  fm <- try(fit(msp, verbose = FALSE))
-
-  if (class(fm) != "try-error") {{
-    ## try to align states across rsvs, naive approach just sorts by emission mean
-    state_order <- c(1, 1 + order(summary(fm)[, 1], decreasing = TRUE))
-    ordered_posterior <- fm@posterior[, state_order]
-    colnames(ordered_posterior) <- c("state", paste0("S", seq_len(K)))
-
-    plot_hmm[[i]] <- cbind(df, ordered_posterior) %>%
-      gather(state_prob, prob, starts_with("S", ignore.case = FALSE)) %>%
-      as_data_frame
-  }
-}
-
-plot_hmm <- do.call(rbind, plot_hmm) %>%
-  left_join(samples)
-plot_hmm$rsv <- factor(plot_hmm$rsv, levels = mix_tree$label)
-
-for (state in paste0("S", 1:K)) {
-  p <- ggplot(plot_hmm %>% filter_(sprintf("state_prob == '%s'", state))) +
-    geom_tile(aes(x = rsv, y = sample, fill = prob)) +
-    scale_fill_gradient(low = "white", high = "black") +
-    facet_grid(ind ~ ., scale = "free_y") +
-    theme(axis.text = element_blank()) +
-    ggtitle(sprintf("State %s", state))
-  print(p)
 }
