f125fc07beed0a824547cc2c8c4e94792e16efa9
Kris Sankaran <sankaran.kris@gmail.com>
2017-07-04 12:01:45 -0700

    make sure not to drop when computing means / covs

--- src/hmm/utils.R
+++ src/hmm/utils.R
@@ -19,8 +19,8 @@ initialize_states <- function(y, L) {
   y_clust <- kmeans(y, L)
   theta <- setNames(vector(mode = "list", length = L), 1:L)
   for (l in seq_len(L)) {
-    theta[[l]]$mu <- colMeans(y[y_clust$cluster == l, ])
-    theta[[l]]$sigma <- 0.75 * cov(y[y_clust$cluster == l, ])
+    theta[[l]]$mu <- colMeans(y[y_clust$cluster == l,, drop = FALSE])
+    theta[[l]]$sigma <- 0.75 * cov(y[y_clust$cluster == l,, drop = FALSE])
   }
 
   list(
