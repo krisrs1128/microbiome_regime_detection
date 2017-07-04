3d7ab8a29178851bbcde021fc80e8caefb8f471a
Kris Sankaran <sankaran.kris@gmail.com>
2017-07-04 13:17:05 -0700

    make sure not to drop into vector

--- src/hmm/bayes_hmm.R
+++ src/hmm/bayes_hmm.R
@@ -34,13 +34,13 @@ block_sampler <- function(y, hyper = list(), lambda = list()) {
     cat(sprintf("iteration %s/%s\n", iter, hyper$n_iter))
 
     for (i in seq_len(ncol(y))) {
-      msg <- messages(Pi, y[, i,], theta)
-      z[, i] <- sample_z(Pi, y[, i,], theta, msg)
+      msg <- messages(Pi, as.matrix(y[, i,, drop = FALSE]), theta)
+      z[, i] <- sample_z(Pi, as.matrix(y[, i,, drop = FALSE]), theta, msg)
     }
 
     Pi <- sample_pi(z, hyper$alpha, hyper$kappa)
     theta <- sample_theta(y, z, theta, lambda, hyper$theta_iter)
-    state <- list(z = z, beta = beta, theta = theta)
+    state <- list("z" = z, "Pi" = Pi, "theta" = theta)
   }
 
   state
