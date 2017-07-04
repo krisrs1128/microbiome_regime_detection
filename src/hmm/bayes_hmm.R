65b1bc2b0af00e25a6aabba0b53e80f2980d611f
Kris Sankaran <sankaran.kris@gmail.com>
2017-07-04 13:30:12 -0700

    save the state at each iteration

--- src/hmm/bayes_hmm.R
+++ src/hmm/bayes_hmm.R
@@ -9,6 +9,7 @@
 
 ## ---- libraries ----
 library("mvnfast")
+library("jsonlite")
 library("MCMCpack")
 source("utils.R")
 Rcpp::sourceCpp("messages.cpp")
@@ -41,6 +42,12 @@ block_sampler <- function(y, hyper = list(), lambda = list()) {
     Pi <- sample_pi(z, hyper$alpha, hyper$kappa)
     theta <- sample_theta(y, z, theta, lambda, hyper$theta_iter)
     state <- list("z" = z, "Pi" = Pi, "theta" = theta)
+
+    cat(
+      sprintf("%s\n", toJSON(c("iter" = iter, state))),
+      file = hyper$outpath,
+      append = iter != 1
+    )
   }
 
   state
@@ -53,7 +60,8 @@ merge_default_hyper <- function(opts = list()) {
     "n_iter" = 500,
     "theta_iter" = 2,
     "alpha" = setNames(rep(1, 4), seq_len(4)),
-    "kappa" = 1
+    "kappa" = 1,
+    "outpath" = file.path(getwd(), "samples.txt")
   )
   modifyList(default_opts, opts)
 }
