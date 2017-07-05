90fc917f6111d4bb0c3be083d66a806b0ca78eb3
Kris Sankaran <sankaran.kris@gmail.com>
2017-07-05 15:00:30 -0700

    Various changes to visualization of hdp / hmm results
    - use viridis scales
    - print out transition probability matrix
    - avoid computing gifs on huge arrays
    - remove legends when distracting

--- src/hmm/antibiotic_hmm.R
+++ src/hmm/antibiotic_hmm.R
@@ -12,9 +12,9 @@
 library("tidyverse")
 library("reshape2")
 library("phyloseq")
+library("viridis")
 library("jsonlite")
 library("stringr")
-source("em_hmm.R")
 theme_set(ggscaffold::min_theme(list(
                         "legend_position" = "right",
                         "border_size" = 0.2
@@ -63,13 +63,14 @@ extract_iteration_data <- function(samp_mcmc, n_iter, n_rsv, n_sample) {
   K <- max(sapply(samp_mcmc, function(x) max(x$z)))
   z <- array(dim = c(n_sample, n_rsv, n_iter))
   zgamma <- array(0, dim = c(n_sample, K, n_rsv, n_iter))
+  pi <- array(0, dim = c(K, K, n_iter))
   mu <- matrix(nrow = n_iter, ncol = K)
   sigma <- matrix(nrow = n_iter, ncol = K)
   for (iter in seq_len(n_iter)) {
     z[,, iter] <- samp_mcmc[[iter]]$z
     mu[iter, ] <- sapply(samp_mcmc[[iter]]$theta, function(x) { x$mu })
     sigma[iter, ] <- sapply(samp_mcmc[[iter]]$theta, function(x) { x$sigma })
-
+    pi[,, iter] <- samp_mcmc[[iter]]$Pi
     for (k in seq_len(K)) {
       zgamma[, k,, iter] <- as.integer(z[,, iter] == k)
     }
@@ -78,6 +79,7 @@ extract_iteration_data <- function(samp_mcmc, n_iter, n_rsv, n_sample) {
   list(
     "z" = z,
     "zgamma" = zgamma,
+    "pi" = pi,
     "mu" = mu,
     "sigma" = sigma
   )
@@ -88,17 +90,20 @@ write_gif <- function(mz, name_base) {
     cat(sprintf("saving iteration %s\n", i))
     p <- ggplot(mz %>% filter(iter == i)) +
       geom_tile(
-        aes(x = sample, y = rsv, fill = K)
+        aes(x = sample, y = rsv, fill = as.numeric(K))
       ) +
-      scale_fill_manual(values = cluster_cols) +
-      theme(axis.text = element_blank())
+      scale_fill_viridis(option = "magma") +
+      theme(
+        axis.text = element_blank(),
+        legend.position = "none"
+      )
     ggsave(
       sprintf(
         "figure/%s_iter_%s.png",
         name_base,
         str_pad(i, 4, "left", "0")
       ),
-      p, height = 9, width = 4
+      p, height = 9, width = 3.5
     )
   }
   system(
@@ -109,9 +114,9 @@ write_gif <- function(mz, name_base) {
   )
 }
 
-melt_z <- function(z, x, gamma) {
-  colnames(z) <- colnames(x)
-  rownames(z) <- rownames(x)
+melt_z <- function(z, dimn, gamma) {
+  colnames(z) <- dimn[[3]]
+  rownames(z) <- dimn[[1]]
   mz <- melt(
     z,
     varnames = c("sample", "rsv", "iter"),
@@ -141,29 +146,35 @@ extract_theta <- function(mu) {
 ###############################################################################
 abt <- get(load("../../data/abt.rda")) %>%
   filter_taxa(function(x) { var(x) > 5 }, TRUE)
+samples <- sample_data(abt) %>%
+  data.frame() %>%
+  rownames_to_column("sample")
+
 res <- get(load("hmm_em.rda"))
 gamma <- res$gamma
-gamma <- melt_gamma(gamma, dimn, samples, res$theta)
-K <- nrow(res$Pi)
-dimn <- list(sample_names(abt), seq_len(K), taxa_names(abt))
+K <- nrow(res$pi)
 cluster_cols <- col_fun(K)
+dimn <- list(sample_names(abt), seq_len(K), taxa_names(abt))
+gamma <- melt_gamma(gamma, dimn, samples, res$theta)
 
-ggplot(gamma) +
+p <- ggplot(gamma) +
   geom_tile(
-    aes(x = sample, y = rsv, alpha = gamma, fill = K)
+    aes(x = sample, y = rsv, alpha = gamma, fill = as.numeric(K))
   ) +
-  scale_fill_manual(values = cluster_cols) +
+  scale_fill_viridis(option = "magma") +
   scale_alpha_continuous(range = c(0, 1)) +
-  theme(axis.text = element_blank()) +
+  theme(axis.text = element_blank(), legend.position = "none") +
   facet_grid(K ~ ind, space = "free", scales = "free")
+ggsave("~/Desktop/lab_meetings/20170705/figure/hmm_probs.pdf", height = 3, width = 2)
 
-ggplot(gamma_mode(gamma)) +
+p <- ggplot(gamma_mode(gamma)) +
   geom_tile(
-    aes(x = sample, y = rsv, fill = k_max)
+    aes(x = sample, y = rsv, fill = as.numeric(k_max))
   ) +
-  scale_fill_manual(values = cluster_cols) +
-  theme(axis.text = element_blank()) +
+  scale_fill_viridis(option = "magma") +
+  theme(axis.text = element_blank(), legend.position = "none") +
   facet_grid(. ~ ind, space = "free", scales = "free")
+ggsave("~/Desktop/lab_meetings/20170705/figure/hmm_mode.pdf", height = 3, width = 2)
 
 rownames(res$pi) <- 1:K
 colnames(res$pi) <- 1:K
@@ -173,19 +184,21 @@ round(res$pi[levels(gamma$K), levels(gamma$K)], 3)
 ## Block sampler for bayesian HMM
 ###############################################################################
 samp_mcmc <- readLines("bayes_kappa_4.txt")
-samp_mcmc <- samp_mcmc[seq(1, length(samp_mcmc), 10)] %>%
+samp_mcmc <- samp_mcmc[seq(1, length(samp_mcmc), 1)] %>%
   lapply(fromJSON)
 K <- nrow(samp_mcmc[[1]]$Pi)
 dimn <- list(sample_names(abt), seq_len(K), taxa_names(abt))
 cluster_cols <- col_fun(K)
 
 samp_data <- extract_iteration_data(
- rsamp_mcmc,
+  samp_mcmc,
   length(samp_mcmc),
-  ncol(y),
-  nrow(y)
+  ntaxa(abt),
+  nsamples(abt)
 )
+rm(samp_mcmc)
 gamma <- apply(samp_data$zgamma, c(1, 2, 3), mean)
+samp_data$zgamma <- NULL
 
 theta <- extract_theta(samp_data$mu)
 gamma <- melt_gamma(gamma, dimn, samples, theta$theta)
@@ -194,9 +207,40 @@ theta$mu_df$K <- factor(theta$mu_df$K, levels(gamma$K))
 ggplot(theta$mu_df) +
   geom_histogram(
     aes(x = mu, fill = K),
+    binwidth = 0.01
   ) +
   scale_fill_manual(values = cluster_cols) +
-  facet_wrap(~ K)
+  facet_wrap(~ K, scales = "free")
+
+pi <- samp_data$pi %>%
+  melt(
+    varnames = c("i", "j", "iter"),
+    value.name = "pi_ij"
+  )
+pi$i <- factor(pi$i, levels(gamma$K))
+pi$j <- factor(pi$j, levels(gamma$K))
+
+pi_df <- pi %>%
+  group_by(i, j) %>%
+  summarise(pi_mean = mean(pi_ij), se = sd(pi_ij))
+
+pi_mean <- pi_df %>%
+  select(-se) %>%
+  spread(j, pi_mean) %>%
+  ungroup %>%
+  select(-i)
+pi_se <- pi_df %>%
+  select(-pi_mean) %>%
+  spread(j, se) %>%
+  ungroup %>%
+  select(-i)
+
+round(pi_mean, 3)
+round(pi_se, 3)
+
+ggplot(pi) +
+  geom_histogram(aes(x = pi_ij), binwidth = 0.005) +
+  facet_grid(i ~ j)
 
 ggplot(gamma) +
   geom_tile(
@@ -207,22 +251,24 @@ ggplot(gamma) +
   theme(axis.text = element_blank()) +
   facet_grid(K ~ ind, space = "free", scales = "free")
 
-ggplot(gamma_mode(gamma)) +
+p <- ggplot(gamma_mode(gamma)) +
   geom_tile(
-    aes(x = sample, y = rsv, fill = k_max)
+    aes(x = sample, y = rsv, fill = as.numeric(k_max))
   ) +
-  scale_fill_manual(values = cluster_cols) +
-  theme(axis.text = element_blank()) +
+  scale_fill_viridis(option = "magma") +
+  theme(axis.text = element_blank(), legend.position = "none") +
   facet_grid(. ~ ind, space = "free", scales = "free")
+ggsave("~/Desktop/lab_meetings/20170705/figure/bayes_mode.pdf", height = 3, width = 2)
 
-mz <- melt_z(samp_data$z, x, gamma)
-ggplot(mz %>% filter(rsv %in% levels(gamma$rsv)[1:3])) +
+mz <- melt_z(samp_data$z[,, 1000:1050], dimn, gamma)
+p <- ggplot(mz %>% filter(rsv %in% levels(gamma$rsv)[sample(1:600, 4)])) +
   geom_tile(
-    aes(x = sample, y = iter, fill = K)
+    aes(x = sample, y = iter, fill = as.numeric(K))
   ) +
-  scale_fill_manual(values = cluster_cols) +
+  scale_fill_viridis(option = "magma") +
   facet_wrap(~rsv) +
   theme(axis.text = element_blank())
+ggsave("~/Desktop/lab_meetings/20170705/figure/gibbs_samples.pdf", height = 1.8, width = 3)
 write_gif(mz, "bayes_hmm_z")
 
 ###############################################################################
@@ -237,17 +283,18 @@ cluster_cols <- col_fun(K)
 samp_data <- extract_iteration_data(
   samp_mcmc,
   length(samp_mcmc),
-  ncol(y),
-  nrow(y)
+  ntaxa(abt),
+  nsamples(abt)
 )
-gamma <- apply(samp_data$zgamma[,,, 100:200], c(1, 2, 3), mean)
+rm(samp_mcmc)
+gamma <- apply(samp_data$zgamma, c(1, 2, 3), mean)
+samp_data$zgamma <- NULL
 
 theta <- extract_theta(samp_data$mu)
 dimn[[2]] <- as.character(seq_len(ncol(gamma)))
 gamma <- melt_gamma(gamma, dimn, samples, theta$theta)
 
 theta$mu_df$K <- factor(theta$mu_df$K, levels(gamma$K))
-cluster_cols <- col_fun(length(unique(gamma$K)))
 ggplot(theta$mu_df) +
   geom_histogram(
     aes(x = mu, fill = K),
@@ -264,20 +311,50 @@ ggplot(gamma) +
   theme(axis.text = element_blank()) +
   facet_grid(K ~ ind, space = "free", scales = "free")
 
-ggplot(gamma_mode(gamma)) +
+p <- ggplot(gamma_mode(gamma)) +
   geom_tile(
-    aes(x = sample, y = rsv, fill = k_max)
+    aes(x = sample, y = rsv, fill = as.numeric(k_max))
   ) +
-  scale_fill_manual(values = cluster_cols) +
-  theme(axis.text = element_blank()) +
+  scale_fill_viridis(option = "magma") +
+  theme(axis.text = element_blank(), legend.position = "none") +
   facet_grid(. ~ ind, space = "free", scales = "free")
+ggsave("~/Desktop/lab_meetings/20170705/figure/hdp_antibiotics_mode.pdf", height = 3, width = 2)
 
-mz <- melt_z(samp_data$z, x, gamma)
+## study mixing in z
+mz <- melt_z(samp_data$z[,, 1000:1050], dimn, gamma)
 ggplot(mz %>% filter(rsv %in% levels(gamma$rsv)[1:3])) +
   geom_tile(
-    aes(x = sample, y = iter, fill = K)
+    aes(x = sample, y = iter, fill = as.numeric(K))
   ) +
-  scale_fill_manual(values = cluster_cols) +
+  scale_fill_viridis(option = "magma") +
   facet_wrap(~rsv) +
-  theme(axis.text = element_blank())
+  theme(axis.text = element_blank(),
+        legend.position = "none")
 write_gif(mz, "hdp_hmm_z")
+
+## look at transition probabilities
+pi <- samp_data$pi %>%
+  melt(
+    varnames = c("i", "j", "iter"),
+    value.name = "pi_ij"
+  )
+pi$i <- factor(pi$i, levels(gamma$K))
+pi$j <- factor(pi$j, levels(gamma$K))
+
+pi_df <- pi %>%
+  group_by(i, j) %>%
+  summarise(pi_mean = mean(pi_ij), se = sd(pi_ij))
+
+pi_mean <- pi_df %>%
+  select(-se) %>%
+  spread(j, pi_mean) %>%
+  ungroup %>%
+  select(-i)
+pi_se <- pi_df %>%
+  select(-pi_mean) %>%
+  spread(j, se) %>%
+  ungroup %>%
+  select(-i)
+
+round(pi_mean, 3)
+round(pi_se, 3)
