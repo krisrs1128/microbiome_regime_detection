72b7620fca69bf7cbcfa8baf8de7f724e225df32
Kris Sankaran <sankaran.kris@gmail.com>
2017-10-14 14:29:48 -0700

    only write the thinned gibbs samples

--- src/slds/slds.py
+++ src/slds/slds.py
@@ -28,16 +28,16 @@ def slds_fit(y, n_iter, K=3, D_latent=1, outdir="."):
         model.add_data(y[i, ].reshape(y.shape[1], 1))
 
         for iter in range(n_iter):
+            model.resample_model()
             if (iter % 5 == 0):
                 print(
                     "\t".join([str(s) for s in ["sequence", i, "iter ", iter]])
                 )
 
-            model.resample_model()
-            write_parameters(i, iter, model.emission_distns, outpaths[0], ["C", "R"])
-            write_parameters(i, iter, model.dynamics_distns, outpaths[1], ["A", "Q"])
-            write_states(i, iter, model.stateseqs, outpaths[2])
-            write_rvs(i, iter, model.rvs(), outpaths[3])
+                write_parameters(i, iter, model.emission_distns, outpaths[0], ["C", "R"])
+                write_parameters(i, iter, model.dynamics_distns, outpaths[1], ["A", "Q"])
+                write_states(i, iter, model.stateseqs, outpaths[2])
+                write_rvs(i, iter, model.rvs(), outpaths[3])
 
 
 def write_states(i, iter, stateseqs, outpath):
@@ -68,4 +68,4 @@ def write_parameters(i, iter, reglist, outpath, pnames):
 ##  load data and run models
 ###############################################################################
 y = np.loadtxt("../../data/slds/abt.csv", delimiter=",")
-slds_fit(y[1:10, :], 20, 3, 1, "../../data/slds/")
+slds_fit(y[1:10, :], 200, 3, 1, "../../data/slds/")
